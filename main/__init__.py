32f54e384413906142d53972bba931c6df9ef1f6
Zach Cowe <zach@ACPLASTICS.local>
2022-12-13 16:09:53 -0600

    Template Design and Inheritance Setup

--- main/__init__.py
+++ main/__init__.py
@@ -5,5 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+from main import routes
+
 print(sys.path)
 app.app_context().push()
--- main/routes.py
+++ main/routes.py
@@ -1,1 +1,7 @@
 from main import app
+from flask import render_template
+
+
+@app.route("/")
+def main():
+    return render_template('main.html')
