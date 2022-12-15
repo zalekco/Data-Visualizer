ef86552e802408cc3e71c73b27a9087d87454d3d
Zach Cowe <zach@ACPLASTICS.local>
2022-12-15 15:53:53 -0600

    Static File additions, Route Changes, Test Plot

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,10 @@
 import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import os
+
+
 
 
 app = Flask(__name__)
@@ -9,4 +13,5 @@ app = Flask(__name__)
 from main import routes
 
 print(sys.path)
+print(pd.__version__)
 app.app_context().push()
--- main/routes.py
+++ main/routes.py
@@ -1,7 +1,86 @@
+import io
+import random
 from main import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request, Response
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as py
+import matplotlib as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+#### LOGIC FOR PLOTTING ####
+tickers = [
+            'JPM',
+            'BAC',
+            'C',
+            'WFC',
+            'GS',
+            ]
+
+#Create an empty string called `ticker_string` that we'll add tickers and commas to
+ticker_string = ''
+
+#Loop through every element of `tickers` and add them and a comma to ticker_string
+for ticker in tickers:
+    ticker_string += ticker
+    ticker_string += ','
+    
+#Drop the last comma from `ticker_string`
+ticker_string = ticker_string[:-1]
+
+endpoints = 'chart'
+years = '10'
+HTTP_request = f'https://cloud.iexapis.com/stable/stock/market/batch?symbols={ticker_string}&types={endpoints}&range={years}y&token=sk_a3ddd89fc3684b0ea6fa7b6502aa204f'
 
 
+data = pd.read_json(HTTP_request)
+
+print(data['JPM'] ['chart'])
+
+for ticker in tickers:
+    pd.series_dict.update({ticker : pd.DataFrame(data[ticker]['chart'])['close']})
+
+series_list = []
+
+for ticker in tickers:
+    series_list.append(pd.DataFrame(data[ticker]['chart'])['close'])
+
+series_list.append(pd.DataFrame(data['JPM']['chart'])['date'])
+
+column_names = tickers.copy()
+column_names.append('Date')
+
+data = pd.concat(series_list, axis=1)
+data.columns = column_names
+
+data.set_index('Date', inplace = True)
+
+
+###END LOGIC###
+
+
+###CHART###
+plt.plot()
+
+plt.figure(figsize = (18,12))
+plt.boxplot(data.transpose())
+
+plt.title('Boxplot of Stock Prices (5Y Lookback)', fontsize = 20)
+plt.xlabel('Bank', fontsize=20)
+plt.ylabel('Stock Prices', fontsize = 20)
+
+ticks = range(1, len(data.columns)+1)
+labels = list(data.columns)
+plt.xticks(ticks, labels, fontsize=20)
+
+plt.savefig('static/images/data.png')
+
+
+### END CHART ###
+
+
+#### APP ROUTES #####
 @app.route("/")
 def main():
     return render_template('main.html')
@@ -9,4 +88,4 @@ def main():
 
 @app.route("/display")
 def plot():
-    return render_template('display.html')
+    return render_template('display.html', name = 'data', url='/static/images/data.png')
