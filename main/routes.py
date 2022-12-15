5140cf699a4b8b1bc9443d5580142842d1fa5f38
Zach Cowe <zach@ACPLASTICS.local>
2022-12-15 10:10:45 -0600

    Display Creation

--- main/routes.py
+++ main/routes.py
@@ -5,3 +5,8 @@ from flask import render_template, url_for
 @app.route("/")
 def main():
     return render_template('main.html')
+
+
+@app.route("/display")
+def plot():
+    return render_template('display.html')
